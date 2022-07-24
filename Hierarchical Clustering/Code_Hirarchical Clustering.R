5375e341088dc77540f9c0ad1a2311dca1823c22
WiseStar282 <87527087+WiseStar282@users.noreply.github.com>
2022-07-24 22:54:08 +0700

    Update Code_Hirarchical Clustering.R

--- Hierarchical Clustering/Code_Hirarchical Clustering.R
+++ Hierarchical Clustering/Code_Hirarchical Clustering.R
@@ -1,16 +1,20 @@
+#Library
 library(cluster)
 library(dendextend)
 library(factoextra)
 library(car)
+
 #menginput dataset
 CSGOComplete <- read_csv("C:/Users/ACER/Downloads/Compressed/CSGOComplete.csv")
 csgo <- CSGOComplete[c(1:40),]
 csgob <- csgo[,c(6:16)]
-#uji multikolinearitas
-## Uji asumsi multikolinearitas
+
+# Uji asumsi multikolinearitas
 cor(csgob)
-#menghitung jarak antar data
+
+#Menghitung jarak antar data
 jarak <- dist(csgob)
+
 ## Perbandingan korelasi antar metode hirarki
 # Single
 hc1 = hclust(jarak, 'single')
@@ -27,19 +31,24 @@ hc3 = hclust(jarak,"complete")
 d3 = cophenetic(hc3)
 cor.comp = cor (jarak,d3)
 cor.comp
+
 ## Analisis cluster dgn hirarki average
 hirarki.ave=hclust(jarak,method = "ave")
 hirarki.ave
+
 # dendrogram
 plot(hirarki.ave, labels = csgo$Map)
+
 #jumlah cluster yang optimal
 fviz_nbclust(csgob, hcut, method = 'silhouette')
 #karena hasil optimal = 2, maka k = 2
 plot(hirarki.ave, labels = csgo$Map)
 rect.hclust(hirarki.ave, k = 2, border = 2:3)
+
 ## Anggota cluster
 hasil.cut = cutree(hirarki.ave,2)
 table(hasil.cut)
+
 #melihat siapa saja anggota cluster
 row.names(csgob) [hasil.cut == 1]
 labels=csgo$Map [hasil.cut == 1] ; labels
